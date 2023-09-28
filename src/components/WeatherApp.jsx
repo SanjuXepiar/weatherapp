7750b05a55895fde77c96fb5cbeab0b51908d6a5
SanjuXepiar <sanjusarkar238@gmail.com>
2023-09-28 16:53:53 +0530

    weatherapp

---
+++ src/components/WeatherApp.jsx
@@ -0,0 +1,153 @@
+import React, { useState } from "react";
+import "./WeatherApp.css";
+import searchIcon from "../assets/search.gif";
+import clearIcon from "../assets/clear.png";
+import thunderIcon from "../assets/thunder.png";
+import rainfallIcon from "../assets/rainfall.png";
+import cloudyIcon from "../assets/cloudy.png";
+import snowIcon from "../assets/snow.png";
+import humidityIcon from "../assets/humidity1.png";
+import windIcon from "../assets/wind.png";
+const WeatherApp = () => {
+  const [input, setInput] = useState("");
+  const [temperature, setTemperature] = useState("33.97 \u00b0C");
+  const [location, setLocation] = useState("Kolkata");
+  const [relativeHumidity, setRelativeHumidity] = useState("66%");
+  const [windSpeed, setWindSpeed] = useState("2.06 km/h");
+  const [alert, setAlert] = useState("");
+  const [weatherIcon, setWeatherIcon] = useState(cloudyIcon);
+  //
+  let apiKey = "72ce6b8e96c77393a772cb2885a935e8";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&units=Metric&appid=${apiKey}`;
+  const handleSearch = async () => {
+    if (input.length === 0) {
+      return 0;
+    } else {
+      try {
+        const response = await fetch(url);
+        if (response.ok) {
+          const data = await response.json();
+          setTemperature(`${data.main.temp} \u00b0C`);
+          setLocation(data.name);
+          setRelativeHumidity(`${data.main.humidity}%`);
+          setWindSpeed(`${data.wind.speed} km/h`);
+          if (data.weather.icon === "01d" || data.weather.icon === "01n") {
+            setWeatherIcon(clearIcon);
+          } else if (
+            data.weather.icon === "02d" ||
+            data.weather.icon === "02n"
+          ) {
+            setWeatherIcon(cloudyIcon);
+          } else if (
+            data.weather.icon === "03d" ||
+            data.weather.icon === "03n"
+          ) {
+            setWeatherIcon(thunderIcon);
+          } else if (
+            data.weather.icon === "04d" ||
+            data.weather.icon === "04n"
+          ) {
+            setWeatherIcon(thunderIcon);
+          } else if (
+            data.weather.icon === "09d" ||
+            data.weather.icon === "09n"
+          ) {
+            setWeatherIcon(rainfallIcon);
+          } else if (
+            data.weather.icon === "10d" ||
+            data.weather.icon === "10n"
+          ) {
+            setWeatherIcon(rainfallIcon);
+          } else if (
+            data.weather.icon === "13d" ||
+            data.weather.icon === "13n"
+          ) {
+            setWeatherIcon(snowIcon);
+          } else {
+            setWeatherIcon(clearIcon);
+          }
+        } else {
+          //   console.error(`HTTP Error: ${response.status}`);
+          setAlert(` error${response.status}: Invlid Input`);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    setTimeout(() => {
+      setInput("");
+      setAlert("");
+    }, 2000);
+  };
+
+  return (
+    <div>
+      <main>
+        {/* contains the Input and the Search Icons */}
+        <section className="header">
+          <div className="weather_Search">
+            <input
+              type="text"
+              placeholder="Search here..."
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+            />
+            <img
+              src={searchIcon}
+              alt=""
+              style={{ width: "3rem", borderRadius: "45%" }}
+              onClick={handleSearch}
+            />
+          </div>
+        </section>
+        <p
+          style={{
+            color: "#eeff00",
+            height: "1rem",
+            marginTop: "0",
+            fontSize: "2rem",
+          }}
+        >
+          {alert}
+        </p>
+        {/* contains the Weather_Icon, Temperature, Location */}
+        <section className="body">
+          <div className="weather_Info">
+            <img src={weatherIcon} alt="" className="weatherIcon" />
+            <span id="temperature">{temperature}</span>
+            <h4 id="location">{location}</h4>
+          </div>
+        </section>
+        {/* contains humidity and wind description */}
+        <section className="footer">
+          <div className="weather_Desc">
+            <div className="humidity_Info">
+              <img
+                src={humidityIcon}
+                alt=""
+                style={{ width: "3rem", height: "3rem" }}
+              />
+              <div className="humidity_Desc">
+                <span id="relativeHumidity">{relativeHumidity}</span>
+                <h5 style={{ marginTop: "0" }}>Humidity</h5>
+              </div>
+            </div>
+            <div className="wind_Info">
+              <img
+                src={windIcon}
+                alt=""
+                style={{ width: "3rem", height: "3rem" }}
+              />
+              <div className="wind_Desc">
+                <span id="windSpeed">{windSpeed}</span>
+                <h5 style={{ marginTop: "0" }}>Wind Speed</h5>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default WeatherApp;
